02732bc2a2089d6f42a69f657c9f24e86400a86a
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-12 04:42:59 -0700

    init commit

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super();
+
+    this.state = { secondsLeft: props.initialCount };
+  }
+  render() {
+    return (
+      <div>
+        <p>
+          {this.props.initialCount ? this.props.initialCount : ''}
+          {this.state.secondsLeft ? ' seconds left before I go b' : 'B'}
+          oom!
+        </p>
+      </div>
+    );
+  }
+}
